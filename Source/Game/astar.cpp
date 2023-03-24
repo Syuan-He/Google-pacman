2ae7b9c159f1d3862788ba56771d7e66edb1aaa0
ntut-hsh <126324937+ntut-hsh@users.noreply.github.com>
2023-03-24 11:01:33 +0800

    delete debug folder

--- Source/Game/astar.cpp
+++ Source/Game/astar.cpp
@@ -35,50 +35,49 @@ bool CGameStateRun::isVaildNode(int x, int y, int xx, int yy) {
 int CGameStateRun::astar(int x0, int y0, int x1, int y1) {
 	if (x0 == x1 && y0 == y1) {
 		return 4;
-	}else{
-		// set some necessary array
-		bool close[sizeof(gameMap[0]) / sizeof(gameMap[0][0])][sizeof(gameMap) / sizeof(gameMap[0])] = {};
-		int valueF[sizeof(gameMap[0]) / sizeof(gameMap[0][0])][sizeof(gameMap) / sizeof(gameMap[0])] = {};
-		priority_queue<Node> open;
-		int pre[sizeof(gameMap[0]) / sizeof(gameMap[0][0])][sizeof(gameMap) / sizeof(gameMap[0])][3] = {};
-		const int nextPos[4][2] = { {1,0},{0,-1},{-1,0},{0,1} };
 
-		//set some variable
-		Node node(x0, y0);
-		node.g = 0;
-		node.h = manhattan(x0, y0, x1, y1);
-		node.f = node.g + node.h;
-		valueF[x0][y0] = node.f;
-		open.push(node);
+	// 設定變數
+	bool close[sizeof(gameMap[0]) / sizeof(gameMap[0][0])][sizeof(gameMap) / sizeof(gameMap[0])] = {};
+	int valueF[sizeof(gameMap[0]) / sizeof(gameMap[0][0])][sizeof(gameMap) / sizeof(gameMap[0])] = {};
+	priority_queue<Node> open;
+	int pre[sizeof(gameMap[0]) / sizeof(gameMap[0][0])][sizeof(gameMap) / sizeof(gameMap[0])][3] = {};
+	const int nextPos[4][2] = { {1,0},{0,-1},{-1,0},{0,1} };
+	Node node(x0, y0);
+	node.g = 0;
+	node.h = manhattan(x0, y0, x1, y1);
+	node.f = node.g + node.h;
+	valueF[x0][y0] = node.f;
+	open.push(node);
 
-		//main
-		while (!open.empty()) {
-			Node node_current = open.top();
-			open.pop();
-			close[node_current.x][node_current.y] = true;
-			if (node_current.x == x1 && node_current.y == y1)
-				break;
-			for (int i = 0; i < 4; i++) {
-				Node node_next(node_current.x + nextPos[i][0], node_current.y + nextPos[i][1]);
-				if (isVaildNode(node_next.x, node_next.y, node_current.x, node_current.y) && !close[node_next.x][node_next.y]) {
-					node_next.g = node_current.g + int(sqrt(pow(nextPos[i][0], 2)) + sqrt(pow(nextPos[i][1], 2)));
-					node_next.h = manhattan(node_next.x, node.y, x1, y1);
-					node_next.f = node_next.g + node_next.h;
-					if (node_next.f < valueF[node_next.x][node_next.y] || valueF[node_next.x][node_next.y] == 0) {
+	//main
+	while (!open.empty()) {
+		Node node_current = open.top();
+		open.pop();
+		close[node_current.x][node_current.y] = true;
+		if (node_current.x == x1 && node_current.y == y1)
+			break;
+		for (int i = 0; i < 4; i++) {
+			//偵測周圍4個點是否可以走
+			Node node_next(node_current.x + nextPos[i][0], node_current.y + nextPos[i][1]);
+			if (isVaildNode(node_next.x, node_next.y, node_current.x, node_current.y) && !close[node_next.x][node_next.y]) {
+				node_next.g = node_current.g + int(sqrt(pow(nextPos[i][0], 2)) + sqrt(pow(nextPos[i][1], 2)));
+				node_next.h = manhattan(node_next.x, node.y, x1, y1);
+				node_next.f = node_next.g + node_next.h;
+				if (node_next.f < valueF[node_next.x][node_next.y] || valueF[node_next.x][node_next.y] == 0) {
 
-						pre[node_next.x][node_next.y][0] = node_current.x;
-						pre[node_next.x][node_next.y][1] = node_current.y;
-						pre[node_current.x][node_current.y][2] = i;
-						valueF[node_next.x][node_next.y] = node_next.f;
-						open.push(node_next);
-					}
+					pre[node_next.x][node_next.y][0] = node_current.x;
+					pre[node_next.x][node_next.y][1] = node_current.y;
+					pre[node_current.x][node_current.y][2] = i;
+					valueF[node_next.x][node_next.y] = node_next.f;
+					open.push(node_next);
 				}
 			}
 		}
-		int x = x1, y = y1, count = 0;
+		int x = x1, y = y1, count = 0, temp;
 		while (pre[x][y][0] != x0 || pre[x][y][1] != y0) {
-			x = pre[x][y][0];
+			temp = pre[x][y][0];
 			y = pre[x][y][1];
+			x = temp;
 			count++;
 			if (count >= 1000)
 				break;
